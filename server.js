15c1b64b21e552acf88c89e5feca02d45b800af8
Nandini0387 <137043974+Nandini0387@users.noreply.github.com>
2025-08-07 19:49:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -55,5 +55,4 @@ app.post('/updatePortfolioHistory', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
-console.log("Server running on port ${PORT}")
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
